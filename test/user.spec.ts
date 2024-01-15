bbd43bf455e37eff98a0aa21003b53691b0f8928
Alessandro0003 <alessandroquinta3@gmail.com>
2024-01-15 18:35:50 -0300

    :sparkles: test: ensure UsersRoutes return in success creation users in test

---
+++ test/user.spec.ts
@@ -0,0 +1,29 @@
+import { it, beforeAll, afterAll, describe, beforeEach } from 'vitest'
+import { app } from '../src/app'
+import { execSync } from 'node:child_process'
+import request from 'supertest'
+
+describe('Users Routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('Should be able to create a new user', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Ale',
+        email: 'ale@gmail.com'
+      })
+      .expect(200)
+  })
+})
